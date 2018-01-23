5922ef5260ad4074fc5ca9d9f9f66ac046c0a462
jisidro0927 <james.isidro0927@gmail.com>
2018-01-22 16:09:29 -0800

    remove posi = 0;

--- HardwareDebugging.java
+++ HardwareDebugging.java
@@ -110,7 +110,6 @@ public class HardwareDebugging extends OpMode
         // Setup a variable for each Power1 wheel to save power level for telemetry
         double Motor1Power;
         double Motor2Power;
-        double Posi = 0;
         double Posi = servo1.getPosition();
 
 
@@ -143,7 +142,6 @@ public class HardwareDebugging extends OpMode
         dc_Motor1.setPower(Motor1Power);
         dc_Motor2.setPower(Motor2Power);
         servo1.setPosition(Posi);
-        servo2.setPosition(-Posi);
         servo2.setPosition(Posi);
         double Servo1 = servo1.getPosition();
         double Servo2 = servo2.getPosition();
@@ -154,7 +152,6 @@ public class HardwareDebugging extends OpMode
         // Will HardwareMap.get( class, device) send an ID Number???
         telemetry.addData("Motor ID", "Motor1:(%.2f), Motor2:(%.2f)", dc_Motor1, dc_Motor2);
 
-        telemetry.addData("Servos", "servo1 ($.2f), servo2 (%.2f)", Servo1 , Servo2);
         telemetry.addData("Servos", "Servo1 ($.2f), Servo2 (%.2f)", Servo1 , Servo2);
 
     }
